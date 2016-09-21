5dad68aab4a03289e0ba35c8f0cff6d47f334215
Samuel Kerr <Sajoe@users.noreply.github.com>
2016-09-21 04:57:33 -0700

    Update clutch.rb

--- lib/clutch.rb
+++ lib/clutch.rb
@@ -9,8 +9,25 @@ require 'rb-inotify'
 BEGIN{
 $hostname = `hostname`.chomp!
 $options = {}
+<<EOL
+    {one line to give the program's name and a brief idea of what it does.}
+    Copyright (C) {year}  {name of author}
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ## insert optionparser code instead of argv reads
+EOL
+
 $options[:host] = ARGV[0] || '10.0.1.75'
 $options[:db] = 1
 $options[:port] = ARGV[1] || '6379'
