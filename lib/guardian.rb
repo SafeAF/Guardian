d0879e5d762f8908811628424040032da0ef5b28
EntropyLost <transiencymail@gmail.com>
2017-02-16 19:31:26 -0800

    more cleanup

--- lib/guardian.rb
+++ lib/guardian.rb
@@ -6,24 +6,7 @@ require 'thread'
 require 'rb-inotify'
 #require 'drb'
 #require 'rinda'
-<<EOL
-    {one line to give the program's name and a brief idea of what it does.}
-    Copyright (C) {year}  {name of author}
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-## insert optionparser code instead of argv reads
-EOL
 BEGIN{
 $hostname = `hostname`.chomp!
 $options = {}
