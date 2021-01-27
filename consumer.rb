023a66a9ac5188fa2d4e07f7a469fd700bf4cc9d
Samuel Kerr <transiencymail@gmail.com>
2021-01-27 08:30:52 -0700

    added activerecord to consumer.rb

--- consumer.rb
+++ consumer.rb
@@ -1,8 +1,19 @@
-
+require 'active_record'
+require 'sqlite3'
 require 'redis'
 require 'json'
+require 'pry' # use binding.pry for debugging
+
+ENV['RAILS_ENV'] = 'development'
+require './dashboard/config/environment.rb'
 
 $redis = Redis.new(:timeout => 0)
+
+ActiveRecord::Base.establish_connection(
+  adapter: 'sqlite3',
+  database: ':memory:')
+  
+#binding.pry
 
 # need a list of hosts to subcribe to
 $redis.subscribe('guardian') do |on|
