5de75c93148a6be28763740d8882c12610d8b722
Samuel Kerr <transiencymail@gmail.com>
2021-01-27 08:55:20 -0700

    building out consumer filedeltum insertions

--- consumer.rb
+++ consumer.rb
@@ -12,13 +12,16 @@ $redis = Redis.new(:timeout => 0)
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
   database: 'db/development.sqlite3')
-  
-binding.pry
 
-# need a list of hosts to subcribe to
+binding.pry
 $redis.subscribe('guardian') do |on|
 	on.message do |channel, msg|
 		payload = JSON.parse(msg)
+		if Host.exists?(hostname: payload['host'])
+			h = Host.find_by hostname: payload['host']
+			f = FileDeltum.create!(event_time: payload['time'],
+				filename: payload['name'])
+		end
 		p "#{payload["time"]}: #{payload["host"]} - #{payload["name"]} : #{payload["flags"]}"
 	end
 end
