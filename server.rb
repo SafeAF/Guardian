221e76f63d8b484349dd468007160657d30ac370
Samuel Kerr <transiencymail@gmail.com>
2021-01-28 07:25:30 -0700

    more code cleanup

--- server.rb
+++ server.rb
@@ -13,11 +13,7 @@ end
 post '/post_json' do
     payload = params
     payload = JSON.parse(request.body.read) unless params[:path]
-	#FSChange.create(time: payload["time"], name: payload[:name], flags: payload["flags"])
-#	FSChange.create(Time.now)
 	$redis.publish 'guardian', payload.to_json
-    p "#{payload["time"]}: #{payload["host"]} - #{payload["name"]} : #{payload["flags"]}"
-   
 end
 
 
